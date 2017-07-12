29606f7bdf38c4210d8f5819fa35304629cfa1fb
Riefu <xyang@edianlicai.com>
2017-07-12 10:21:39 +0800

    添加注释

--- src/main/java/ThreadPullApi.java
+++ src/main/java/ThreadPullApi.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import static java.lang.reflect.Array.getByte;
+import static java.util.Arrays.asList;
 
 /**
  * User: Riefu
@@ -21,13 +21,24 @@ public class ThreadPullApi implements Runnable {
 
 
 
-    private static final List<String> urlList = new ArrayList<String>(Arrays.asList("http://121.28.74.78:8020/HospitalInterface/AddUserInfo?ExtUserID=300001&ExtUserPwd=400001&IDCard=130406198103262124&Phone=18630121163&PatientName=张三&GuardianName=张三妈&Sex=女"));
+    private static final List<String> urlList = new ArrayList<String>();
 
+    static {
+        asList("","");//  待访问API地址
+    }
     private   FileOutputStream fos ;
     private  Logger logger = Logger.getLogger(this.getClass());
 
-    private  Map<String,Map> cacheMap = new HashMap<String, Map>();
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    private  Map<String,Map> cacheMap = new HashMap<String, Map>(); //需要缓存的查询结果 已二进制数据缓存至本地文件
+
+
+    /**
+     * 主线程
+     * @param args
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException {/
 
             query();
     }
@@ -35,14 +46,9 @@ public class ThreadPullApi implements Runnable {
     public static void query() throws IOException, ClassNotFoundException {
 
 
-//        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-//
-//        scheduledExecutorService.scheduleWithFixedDelay(new ThreadPullApi(),0,10, TimeUnit.SECONDS);
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(); //构造线程池
 
-        ObjectInputStream objectOutputStream = new ObjectInputStream(new FileInputStream(new File("d:/demo.rbd")));
-        final Map map = (Map) objectOutputStream.readObject();
-
-        System.out.println("ok");
+        scheduledExecutorService.scheduleWithFixedDelay(new ThreadPullApi(),0,5, TimeUnit.SECONDS);// 立即执行  线程执行结束后等待5s继续执行
 
 
     }
@@ -51,7 +57,7 @@ public class ThreadPullApi implements Runnable {
     public void run() {
         // 业务逻辑
         int i = 0;
-        final int count = urlList.size();
+        final int count = urlList.size(); // api列表长度
         for ( ;i<count;i++){
             String url = urlList.get(i);
             // 构造请求
@@ -60,15 +66,15 @@ public class ThreadPullApi implements Runnable {
 
             try {
 
-                final Response response = client.newCall(request).execute();
+                final Response response = client.newCall(request).execute(); //获取响应
                 final String result = response.body().string();
-                final int resultKey = url.hashCode();
-                final Map map = JSON.parseObject(result, Map.class);
+                final int resultKey = url.hashCode();//每个Result的HashCode作为key对应缓存结果
+                final Map map = JSON.parseObject(result, Map.class); //响应反序列化为java对象
 
-                cacheMap.put(result,map);
+                cacheMap.put(result,map);//缓存结果集
 
             } catch (IOException e) {
-                e.printStackTrace();
+                e.printStackTrace();// 请使用logger缓存
             }
         }
 
@@ -77,8 +83,8 @@ public class ThreadPullApi implements Runnable {
             ObjectOutputStream oo = new ObjectOutputStream(bao);
             oo.writeObject(cacheMap);
             final byte[] bytes = bao.toByteArray();
-            final FileChannel channel = new FileOutputStream(new File("d:/demo.rbd")).getChannel();
-            channel.write(ByteBuffer.wrap(bytes));
+            final FileChannel channel = new FileOutputStream(new File("d:/demo.rbd")).getChannel();//缓存文件路径  并非aof方式追加进缓存文件每次都是最新的缓存数据
+            channel.write(ByteBuffer.wrap(bytes));//写进文件
             channel.close();
         } catch (IOException e) {
             e.printStackTrace();
